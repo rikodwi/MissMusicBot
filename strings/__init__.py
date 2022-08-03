8f7fdc673719079be8695c828250183adb700022
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-08-04 00:28:58 +0700

    bahasa

--- strings/__init__.py
+++ strings/__init__.py
@@ -39,7 +39,7 @@ for filename in os.listdir(r"./strings"):
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+            open(r"./strings/langs/id.yml", encoding="utf8")
         )
         languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):

ad3cc37a28d250b754fc9494784ab34dfe3e45c9
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-08-20 01:05:34 +0700

    p

--- MissMusic/plugins/bot/settings.py
+++ MissMusic/plugins/bot/settings.py
@@ -98,7 +98,7 @@ async def settings_back_markup(
             OWNER = None
         buttons = private_panel(_, app.username, OWNER)
         return await CallbackQuery.edit_message_text(
-            _["start_2"].format(MUSIC_BOT_NAME),
+            _["start_2"].format(message.from_user.first_name),
             reply_markup=InlineKeyboardMarkup(buttons),
         )
     else:
